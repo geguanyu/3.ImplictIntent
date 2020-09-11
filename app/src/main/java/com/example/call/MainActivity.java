f9f8899acb52810334cfe6b703c9bce102eb5209
geguanyu <769782478@qq.com>
2020-09-11 19:00:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/call/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.call;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:2016011422"));
+                startActivity(intent);
+            }
+        });
+    }
+}
